741832ee87ef1c362c51d7da3d8210efb0dff1ac
jeyaram1023 <jeyaram.reddy.ece@gmail.com>
2025-06-08 14:36:39 +0530

    Update js_main.js
    if ('serviceWorker' in navigator) {
      navigator.serviceWorker.register('/streetr-seller-app/service-worker.js')
        .then(reg => console.log('✅ Service Worker registered', reg))
        .catch(err => console.error('❌ Service Worker failed:', err));
    }

--- js_main.js
+++ js_main.js
@@ -178,4 +178,10 @@ supabase.auth.onAuthStateChange((event, session) => {
 // Initialize the app
 document.addEventListener('DOMContentLoaded', () => {
     checkAuthState(); // Check auth state when DOM is ready
+
+    if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/streetr-seller-app/service-worker.js')
+    .then(reg => console.log('✅ Service Worker registered', reg))
+    .catch(err => console.error('❌ Service Worker failed:', err));
+    }
 });
